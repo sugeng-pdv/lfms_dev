fd0ae624c93c295789babdc32afbc630e3ecfcf1
root <root@asetnegara.id>
2021-01-05 12:41:51 +0700

    Update chown user api

--- application/config/lman_config.php
+++ application/config/lman_config.php
@@ -2,14 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // URL file statis
-$config['static_file_url'] = 'https://landfunding.asetnegara.id/static/';
+$config['static_file_url'] = 'https://asetnegara.id/lfms_dev/static/';
 //url files
 // $config['static_file_download_url'] = 'https://apps.asetnegara.id/lman-vms/files/';
-$config['static_file_download_url'] = 'https://landfunding.asetnegara.id/files/';
+$config['static_file_download_url'] = 'https://asetnegara.id/lfms_dev/files/';
 //base url
-$config['static_base_url'] = 'https://landfunding.asetnegara.id/';
+$config['static_base_url'] = 'https://asetnegara.id/lfms_dev/';
 // URL API Endpoint
-$config['api_endpoint'] = 'https://landfunding.asetnegara.id/api';
+$config['api_endpoint'] = 'https://asetnegara.id/lfms_dev/api';
 //endpoint untuk emailserver
 $config['email_endpoint'] = 'https://asetnegara.id/mail-api/v1';
 // $config['api_s3'] = 'https://procurement.asetnegara.id/';
